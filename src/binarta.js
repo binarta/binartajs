ef2b6657a58f6073ff33e5f0142b42c016ba76ad
sgo <sgo@thinkerit.be>
2016-03-04 09:41:42 +0100

    you can now wire a ui and subsystems into the binartajs facade.

--- src/binarta.js
+++ src/binarta.js
@@ -1,1 +1,25 @@
-function Binartajs() {}
+function BinartajsFactory() {
+    var binartajs = new Binartajs();
+    var ui;
+
+    this.addUI = function(it) {
+        ui = it;
+    };
+
+    this.addSubSystems = function(it) {
+        transferAttributes(it, binartajs);
+    };
+
+    this.create = function() {
+        return binartajs;
+    };
+
+    function Binartajs() {}
+
+    function transferAttributes(from, to) {
+        Object.keys(from).forEach(function(key) {
+            to[key] = from[key];
+            to[key].ui = ui;
+        });
+    }
+}
--- test/binarta.spec.js
+++ test/binarta.spec.js
@@ -1,7 +1,44 @@
 (function() {
     describe('binartajs', function() {
+        var factory, ui;
+
+        beforeEach(function() {
+            ui = new UI();
+            factory = new BinartajsFactory();
+            factory.addUI(ui);
+        });
+
         it('construct the application facade', function() {
-            expect(new Binartajs()).toBeDefined();
+            expect(factory.create()).toBeDefined();
+        });
+
+        it('subsystems are exposed on the application facade', function() {
+            factory.addSubSystems({greeter:new SynchronousGreeter()});
+            expect(factory.create().greeter.sayHello("World")).toEqual("Hello World!");
+        });
+
+        it('subsystems can interact with the wired ui', function() {
+            factory.addSubSystems({greeter:new AsynchronousGreeter()});
+            factory.create().greeter.sayHello("World");
+            expect(ui.receivedGreeting).toEqual("Hello World!");
         });
     });
+
+    function UI() {
+        this.showGreeting = function(greeting) {
+            this.receivedGreeting = greeting;
+        }
+    }
+
+    function SynchronousGreeter() {
+        this.sayHello = function(who) {
+            return "Hello " + who + "!";
+        }
+    }
+
+    function AsynchronousGreeter() {
+        this.sayHello = function(who) {
+            this.ui.showGreeting("Hello " + who + "!");
+        }
+    }
 })();
