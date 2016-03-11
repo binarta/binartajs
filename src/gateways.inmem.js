90ce25d09fff760dd33d936d17d6c22a01209c09
sgo <sgo@thinkerit.be>
2016-03-11 12:46:43 +0100

    in memory checkpoint gateway can now respond to intiateBillingAgreement invocations.

--- src/gateways.inmem.js
+++ src/gateways.inmem.js
@@ -2,8 +2,15 @@ function BinartaInMemoryGatewaysjs() {
     this.checkpoint = new CheckpointGateway();
 
     function CheckpointGateway() {
-        this.fetchAccountMetadata = function(response) {
-            response.activeAccountMetadata({billing:{complete:false}});
+        this.fetchAccountMetadata = function (response) {
+            response.activeAccountMetadata({billing: {complete: false}});
+        };
+
+        this.initiateBillingAgreement = function (id, ui) {
+            ui.approveBillingAgreement({
+                paymentProvider: id,
+                url: 'http://' + id + '/billing/agreement?token=t'
+            });
         }
     }
 }
