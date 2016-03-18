ef5c77b1bb8e6b004b8bc214227c686471129efc
sgo <sgo@thinkerit.be>
2016-03-18 17:18:30 +0100

    junit test report support

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,16 @@
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
-        basePath:'.',
-        frameworks:['jasmine'],
-        files:[
-            {pattern:'src/**/*.js'},
-            {pattern:'test/**/*.js'}
+        basePath: '.',
+        frameworks: ['jasmine'],
+        files: [
+            {pattern: 'src/**/*.js'},
+            {pattern: 'test/**/*.js'}
         ],
-        browsers:['PhantomJS']
+        browsers: ['PhantomJS'],
+        reporters: ['dots', 'junit'],
+        junitReporter: {
+            outputFile: 'test-results.xml',
+            useBrowserName: false
+        }
     });
 };
