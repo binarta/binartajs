67a026fb12f35c6ee9bdf50cc009c545e7ec8daf
sgo <sgo@thinkerit.be>
2016-08-08 12:00:14 +0200

    Importing locale selection and resolution logic from i18n.js

--- src/application.js
+++ src/application.js
@@ -1,16 +1,37 @@
 function BinartaApplicationjs() {
     var app = this;
     var profileCache = {};
+    var cachedLocale;
 
     app.profile = function() {
         return profileCache;
     };
 
+    app.locale = function() {
+        return cachedLocale;
+    };
+
+    app.setLocale = function(locale) {
+        localStorage.locale = locale;
+        sessionStorage.locale = locale;
+        cachedLocale = locale;
+    };
+
     app.refresh = function(onSuccess) {
+        refreshLocale();
+        refreshApplicationProfile(onSuccess);
+    };
+
+    function refreshApplicationProfile(onSuccess) {
         app.gateway.fetchApplicationProfile({}, {success:function(profile) {
             profileCache = profile;
             if(onSuccess)
                 onSuccess();
         }});
+    }
+
+    function refreshLocale() {
+        cachedLocale = sessionStorage.locale || localStorage.locale || undefined;
+        sessionStorage.locale = cachedLocale;
     }
 }
--- test/application.spec.js
+++ test/application.spec.js
@@ -2,12 +2,18 @@
     describe('binarta-applicationjs', function () {
         var binarta, ui;
 
+        beforeEach(function() {
+            localStorage.removeItem('locale');
+            sessionStorage.removeItem('locale');
+        });
         beforeEach(function () {
             ui = new UI();
             var factory = new BinartajsFactory();
             factory.addUI(ui);
             factory.addSubSystems({application: new BinartaApplicationjs()});
             binarta = factory.create();
+
+            binarta.application.gateway = new ValidApplicationGateway();
         });
 
         it('exposes an empty profile', function () {
@@ -43,6 +49,85 @@
                 binarta.application.gateway = new ValidApplicationGateway();
                 binarta.application.refresh(spy);
                 expect(spy).toHaveBeenCalled();
+            });
+        });
+
+        describe('locale resolution', function () {
+            it('starts out undefined', function () {
+                expect(binarta.application.locale()).toBeUndefined();
+            });
+
+            describe('when locale is specified in local storage', function () {
+                beforeEach(function () {
+                    localStorage.locale = 'from-local-storage';
+                    binarta.application.refresh();
+                });
+
+                it('then resolve language from local storage', function () {
+                    expect(binarta.application.locale()).toEqual('from-local-storage');
+                });
+
+                describe('and locale is specified in session storage', function () {
+                    beforeEach(function () {
+                        sessionStorage.locale = 'from-session-storage';
+                        binarta.application.refresh();
+                    });
+
+                    it('then resolves from session storage', function () {
+                        expect(binarta.application.locale()).toEqual('from-session-storage');
+                    });
+
+                    describe('and session storage is cleared', function () {
+                        beforeEach(function () {
+                            sessionStorage.removeItem('locale');
+                            binarta.application.refresh();
+                        });
+
+                        it('then resolves from local storage', function () {
+                            expect(binarta.application.locale()).toEqual('from-local-storage');
+                        });
+                    });
+
+                    describe('and has previously been resolved', function () {
+                        beforeEach(function () {
+                            sessionStorage.locale = 'from-memory';
+                            binarta.application.refresh();
+                        });
+
+                        describe('and session storage and local storage are cleared', function () {
+                            beforeEach(function () {
+                                delete sessionStorage.locale;
+                                delete localStorage.locale;
+                            });
+
+                            it('then resolves to remembered locale', function () {
+                                expect(binarta.application.locale()).toEqual('from-memory');
+                            });
+                        });
+                    });
+                });
+
+                it('then local storage locale is promoted to session storage locale', function () {
+                    expect(sessionStorage.locale).toEqual(localStorage.locale);
+                });
+            });
+
+            describe('when swapped locale', function () {
+                beforeEach(function () {
+                    binarta.application.setLocale('swapped-locale');
+                });
+
+                it('then locale is saved in local storage', function () {
+                    expect(localStorage.locale).toEqual('swapped-locale');
+                });
+
+                it('then locale is saved in session storage', function () {
+                    expect(sessionStorage.locale).toEqual('swapped-locale');
+                });
+
+                it('then locale resolves without refresh', function() {
+                    expect(binarta.application.locale()).toEqual('swapped-locale');
+                });
             });
         });
     });
