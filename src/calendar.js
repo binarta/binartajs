2d586a1cac4fb019c900d5c0c860661e899d71e5
sgo <sgo@thinkerit.be>
2019-01-17 11:39:59 +0100

    Reworked the calendar.upcoming widget so components are notified when the events are refreshed and subscribing an observer will trigger a refresh if it hasn't happened before.

--- src/calendar.js
+++ src/calendar.js
@@ -4,19 +4,38 @@ function BinartaCalendarjs() {
     calendar.upcoming = new Upcoming();
 
     function Upcoming() {
+        var rx = new BinartaRX();
         var upcoming = this;
+        var events = [];
+        var loading = false, initialised = false;
 
-        upcoming.events = [];
+        upcoming.observe = function (l) {
+            var o = rx.observe(l);
+            if (!initialised)
+                upcoming.refresh();
+            raiseEvents();
+            return o;
+        };
 
         upcoming.refresh = function () {
-            calendar.gateway.findUpcomingEvents({}, {
-                success: function (it) {
-                    upcoming.events = it.map(function (it) {
-                        it.start = moment(it.start);
-                        return it;
-                    });
-                }
-            })
+            if(!loading) {
+                loading = true;
+                calendar.gateway.findUpcomingEvents({}, {
+                    success: function (it) {
+                        events = it.map(function (it) {
+                            it.start = moment(it.start);
+                            return it;
+                        });
+                        raiseEvents();
+                        initialised = true;
+                        loading = false;
+                    }
+                });
+            }
+        };
+
+        function raiseEvents() {
+            rx.notify('events', events);
         }
     }
 }
--- test/calendar.spec.js
+++ test/calendar.spec.js
@@ -14,12 +14,16 @@
         });
 
         describe('upcoming events', function () {
+            var response, observer;
+
             beforeEach(function () {
+                response = jasmine.createSpyObj('response', ['events']);
                 calendar.gateway = new ValidCalendarGateway();
             });
 
-            it('are initially empty', function () {
-                expect(calendar.upcoming.events).toEqual([]);
+            afterEach(function () {
+                if (observer)
+                    observer.disconnect();
             });
 
             it('refresh to load upcoming events from server', function () {
@@ -28,13 +32,58 @@
                 expect(calendar.gateway.findUpcomingEventsRequest).toEqual({});
             });
 
-            it('refresh updates the list of upcoming events', function () {
-                calendar.upcoming.refresh();
-                expect(calendar.upcoming.events).toEqual([
+            it('installing an observer loads upcoming events from server', function () {
+                calendar.gateway = new GatewaySpy();
+                observer = calendar.upcoming.observe(response);
+                expect(calendar.gateway.findUpcomingEventsRequest).toEqual({});
+            });
+
+            it('observers receive the list of upcoming events', function () {
+                observer = calendar.upcoming.observe(response);
+                expect(response.events).toHaveBeenCalledWith([
                     {id: 'a', start: moment('2017-02-01T16:00:00Z')},
                     {id: 'b', start: moment('2017-02-02T16:00:00Z')},
                     {id: 'c', start: moment('2017-02-03T16:00:00Z')}
                 ]);
+            });
+
+            it('disconnected observers are not notified of upcoming events', function () {
+                calendar.gateway = new GatewaySpy();
+                observer = calendar.upcoming.observe(response);
+                observer.disconnect();
+                calendar.gateway = new ValidCalendarGateway();
+                calendar.upcoming.refresh();
+                expect(response.events).not.toHaveBeenCalledWith([
+                    {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                    {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                    {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                ]);
+            });
+
+            it('installing additional observers reuses previously loaded events', function () {
+                observer = calendar.upcoming.observe(response);
+                calendar.gateway = {};
+                response.events.calls.reset();
+                calendar.upcoming.observe(response).disconnect();
+                expect(response.events).toHaveBeenCalledWith([
+                    {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                    {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                    {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                ]);
+            });
+
+            it('refreshing before events could be loaded from server does not trigger additional lookups', function () {
+                calendar.gateway = new GatewaySpy();
+                calendar.upcoming.refresh();
+                calendar.gateway = {};
+                calendar.upcoming.refresh();
+            });
+
+            it('refreshing after events could be loaded from performs lookup from server', function () {
+                calendar.upcoming.refresh();
+                calendar.gateway = new GatewaySpy();
+                calendar.upcoming.refresh();
+                expect(calendar.gateway.findUpcomingEventsRequest).toEqual({});
             });
         });
     });
