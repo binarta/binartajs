523abb8545b02a497b1562fd1a7e15489b4ed0ed
sgo <sgo@thinkerit.be>
2019-02-01 11:38:26 +0100

    When asking for upcoming events it's not possible to specify an optional discriminator to filter the events to just those you're interested in.

--- src/calendar.js
+++ src/calendar.js
@@ -5,40 +5,64 @@ function BinartaCalendarjs() {
     calendar.upcoming = new Upcoming();
 
     function Upcoming() {
-        var rx = new BinartaRX();
         var upcoming = this;
-        var events = [];
-        var loading = false, initialised = false;
+        var map = {};
 
-        upcoming.observe = function (l) {
-            var o = rx.observe(l);
-            if (!initialised)
-                upcoming.refresh();
-            raiseEvents();
-            return o;
-        };
+        function Events() {
+            var self = this;
+            var rx = new BinartaRX();
+            var loading = false, initialised = false;
+            var events = [];
 
-        upcoming.refresh = function () {
-            if (!loading) {
-                loading = true;
-                calendar.gateway.findUpcomingEvents({
-                    startDate: moment(calendar.timeline.shift())
-                }, {
-                    success: function (it) {
-                        events = it.map(function (it) {
-                            it.start = moment(it.start);
-                            return it;
-                        });
-                        raiseEvents();
-                        initialised = true;
-                        loading = false;
-                    }
-                });
+            self.observe = function (l, d) {
+                var o = rx.observe(l);
+                if (!initialised)
+                    upcoming.refresh(d);
+                raiseEvents();
+                return o;
+            };
+
+            self.refresh = function (discriminator) {
+                if (!loading) {
+                    loading = true;
+                    var request = {startDate: moment(calendar.timeline.shift())};
+                    if (discriminator)
+                        request.metadata = discriminator;
+                    calendar.gateway.findUpcomingEvents(request, {
+                        success: function (it) {
+                            events = it.map(function (it) {
+                                it.start = moment(it.start);
+                                return it;
+                            });
+                            raiseEvents();
+                            initialised = true;
+                            loading = false;
+                        }
+                    });
+                }
+            };
+
+            function raiseEvents() {
+                rx.notify('events', events);
             }
+        }
+
+        map.all = new Events();
+
+        upcoming.observe = function (l, d) {
+            return events(d).observe(l, d);
         };
 
-        function raiseEvents() {
-            rx.notify('events', events);
+        upcoming.refresh = function (d) {
+            events(d).refresh(d);
+        };
+
+        function events(it) {
+            if(!it)
+                return events('all');
+            if (!map[it])
+                map[it] = new Events();
+            return map[it];
         }
     }
 }
--- test/calendar.spec.js
+++ test/calendar.spec.js
@@ -28,64 +28,143 @@
                     observer.disconnect();
             });
 
-            it('refresh to load upcoming events from server', function () {
-                calendar.gateway = new GatewaySpy();
-                calendar.upcoming.refresh();
-                expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
+            describe('without filtering', function () {
+                it('refresh to load upcoming events from server', function () {
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh();
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
+                });
+
+                it('installing an observer loads upcoming events from server', function () {
+                    calendar.gateway = new GatewaySpy();
+                    observer = calendar.upcoming.observe(response);
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
+                });
+
+                it('observers receive the list of upcoming events', function () {
+                    observer = calendar.upcoming.observe(response);
+                    expect(response.events).toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
+
+                it('disconnected observers are not notified of upcoming events', function () {
+                    calendar.gateway = new GatewaySpy();
+                    observer = calendar.upcoming.observe(response);
+                    observer.disconnect();
+                    calendar.gateway = new ValidCalendarGateway();
+                    calendar.upcoming.refresh();
+                    expect(response.events).not.toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
+
+                it('installing additional observers reuses previously loaded events', function () {
+                    observer = calendar.upcoming.observe(response);
+                    calendar.gateway = {};
+                    response.events.calls.reset();
+                    calendar.upcoming.observe(response).disconnect();
+                    expect(response.events).toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
+
+                it('refreshing before events could be loaded from server does not trigger additional lookups', function () {
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh();
+                    calendar.gateway = {};
+                    calendar.upcoming.refresh();
+                });
+
+                it('refreshing after events could be loaded from performs lookup from server', function () {
+                    calendar.upcoming.refresh();
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh();
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
+                });
             });
 
-            it('installing an observer loads upcoming events from server', function () {
-                calendar.gateway = new GatewaySpy();
-                observer = calendar.upcoming.observe(response);
-                expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
-            });
+            describe('with filtering', function () {
+                var discriminator;
 
-            it('observers receive the list of upcoming events', function () {
-                observer = calendar.upcoming.observe(response);
-                expect(response.events).toHaveBeenCalledWith([
-                    {id: 'a', start: moment('2017-02-01T16:00:00Z')},
-                    {id: 'b', start: moment('2017-02-02T16:00:00Z')},
-                    {id: 'c', start: moment('2017-02-03T16:00:00Z')}
-                ]);
-            });
+                beforeEach(function () {
+                    discriminator = 'd';
+                });
 
-            it('disconnected observers are not notified of upcoming events', function () {
-                calendar.gateway = new GatewaySpy();
-                observer = calendar.upcoming.observe(response);
-                observer.disconnect();
-                calendar.gateway = new ValidCalendarGateway();
-                calendar.upcoming.refresh();
-                expect(response.events).not.toHaveBeenCalledWith([
-                    {id: 'a', start: moment('2017-02-01T16:00:00Z')},
-                    {id: 'b', start: moment('2017-02-02T16:00:00Z')},
-                    {id: 'c', start: moment('2017-02-03T16:00:00Z')}
-                ]);
-            });
+                it('refresh to load upcoming events from server', function () {
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh(discriminator);
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({
+                        startDate: moment(now),
+                        metadata: 'd'
+                    });
+                });
 
-            it('installing additional observers reuses previously loaded events', function () {
-                observer = calendar.upcoming.observe(response);
-                calendar.gateway = {};
-                response.events.calls.reset();
-                calendar.upcoming.observe(response).disconnect();
-                expect(response.events).toHaveBeenCalledWith([
-                    {id: 'a', start: moment('2017-02-01T16:00:00Z')},
-                    {id: 'b', start: moment('2017-02-02T16:00:00Z')},
-                    {id: 'c', start: moment('2017-02-03T16:00:00Z')}
-                ]);
-            });
+                it('installing an observer loads upcoming events from server', function () {
+                    calendar.gateway = new GatewaySpy();
+                    observer = calendar.upcoming.observe(response, discriminator);
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({
+                        startDate: moment(now),
+                        metadata: 'd'
+                    });
+                });
 
-            it('refreshing before events could be loaded from server does not trigger additional lookups', function () {
-                calendar.gateway = new GatewaySpy();
-                calendar.upcoming.refresh();
-                calendar.gateway = {};
-                calendar.upcoming.refresh();
-            });
+                it('observers receive the list of upcoming events', function () {
+                    observer = calendar.upcoming.observe(response, discriminator);
+                    expect(response.events).toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
 
-            it('refreshing after events could be loaded from performs lookup from server', function () {
-                calendar.upcoming.refresh();
-                calendar.gateway = new GatewaySpy();
-                calendar.upcoming.refresh();
-                expect(calendar.gateway.findUpcomingEventsRequest).toEqual({startDate: moment(now)});
+                it('disconnected observers are not notified of upcoming events', function () {
+                    calendar.gateway = new GatewaySpy();
+                    observer = calendar.upcoming.observe(response, discriminator);
+                    observer.disconnect();
+                    calendar.gateway = new ValidCalendarGateway();
+                    calendar.upcoming.refresh(discriminator);
+                    expect(response.events).not.toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
+
+                it('installing additional observers reuses previously loaded events', function () {
+                    observer = calendar.upcoming.observe(response, discriminator);
+                    calendar.gateway = {};
+                    response.events.calls.reset();
+                    calendar.upcoming.observe(response, discriminator).disconnect();
+                    expect(response.events).toHaveBeenCalledWith([
+                        {id: 'a', start: moment('2017-02-01T16:00:00Z')},
+                        {id: 'b', start: moment('2017-02-02T16:00:00Z')},
+                        {id: 'c', start: moment('2017-02-03T16:00:00Z')}
+                    ]);
+                });
+
+                it('refreshing before events could be loaded from server does not trigger additional lookups', function () {
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh(discriminator);
+                    calendar.gateway = {};
+                    calendar.upcoming.refresh(discriminator);
+                });
+
+                it('refreshing after events could be loaded from performs lookup from server', function () {
+                    calendar.upcoming.refresh(discriminator);
+                    calendar.gateway = new GatewaySpy();
+                    calendar.upcoming.refresh(discriminator);
+                    expect(calendar.gateway.findUpcomingEventsRequest).toEqual({
+                        startDate: moment(now),
+                        metadata: 'd'
+                    });
+                });
             });
         });
     });
